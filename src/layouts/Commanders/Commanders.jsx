87a9ce1e013780eaabf2059d90861e4dc96d0ad9
Eduard <eduard66_06@list.ru>
2021-09-07 12:50:53 +1200

    update command data

--- src/layouts/Commanders/Commanders.jsx
+++ src/layouts/Commanders/Commanders.jsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Commander from "../../components/Commander";
+import {setSolder} from "../../store/soldersArr";
+import {useDispatch, useSelector} from "react-redux";
 
 const Commanders = () => {
+  const [counter, setCounter] = useState(1)
+
+  const startSolder = useSelector((state) => state.solders)
+  const dispatch = useDispatch();
+
+  const addCommander = async function () {
+    await dispatch(setSolder({id: counter}))
+    await setCounter(counter + 1);
+  }
+
   return (
     <div>
       <div className="commander__header">
         commander
       </div>
       <Commander />
-      <button  className="soldersButton" >add another commander?</button>
+      <button  className="soldersButton" onClick={addCommander}>add another commander?</button>
     </div>
   );
 };
